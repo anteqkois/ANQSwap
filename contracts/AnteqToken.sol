3e068c21476f92ca2297086e2d1977705944a793
Antek Kois <koisantek96@gmail.com>
2022-01-19 11:02:31 +0100

    add some test and precommit comend to test

--- contracts/AnteqToken.sol
+++ contracts/AnteqToken.sol
@@ -1,20 +1,18 @@
-pragma solidity >= 0.4.21 < 8.11.0;
+pragma solidity >= 0.4.24 < 8.11.0;
 
 contract AnteqToken{
 
   string private _name = "AnteqToken";
-  string public symbol = "ANQ";
-  uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
-  uint256 public owner;
-  uint8   public decimals = 18;
+  string private _symbol = "ANQ";
+  uint256 private _totalSupply = 1000000000000000000000000; // 1 million tokens
+  uint8   private _decimals = 18;
 
-  mapping(address => uint256) public balanceOf;
-  mapping(address => mapping(address => uint256)) public allowance;
+  mapping(address => uint256) private _balanceOf;
+  mapping(address => mapping(address => uint256)) private _allowance;
 
 
   constructor() public {
-    // owner = msg.sender;
-    balanceOf[msg.sender] = totalSupply;
+    _balanceOf[msg.sender] = _totalSupply;
   }
 
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
@@ -24,19 +22,21 @@ contract AnteqToken{
     return _name;
   }
   
-  // function symbol() public view returns (string){
-  //   return symbol;
-  // }
+  function symbol() public view returns (string memory){
+    return _symbol;
+  }
 
-  // function decimals() public view returns (uint8){
-  //   return decimals;
-  // }
+  function totalSupply() public view returns (uint256){
+    return _totalSupply;
+  }
 
-  // function totalSupply() public view returns (uint256){
-  //   return totalSuplly;
-  // }
+  function decimals() public view returns (uint8){
+    return _decimals;
+  }
 
-  // function balanceOf(address _owner) public view returns (uint256 balance)
+  function balanceOf(address _owner) public view returns (uint256 balance){
+    return _balanceOf[_owner];
+  }
   // function transfer(address _to, uint256 _value) public returns (bool success)
   // function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
   // function approve(address _spender, uint256 _value) public returns (bool success)
--- test/anteqtoken.js
+++ test/anteqtoken.js
@@ -1,16 +1,45 @@
 const AnteqToken = artifacts.require('./AnteqToken.sol');
+// const Web3 = require('./client/web3');
 
 contract('AnteqToken', (accounts) => {
-
   let anteqTokenInstance;
 
-  before(async()=>{
+  before(async () => {
     anteqTokenInstance = await AnteqToken.new();
-
-  })
+  });
 
   it('Should name to "AnteqToken".', async () => {
     const name = await anteqTokenInstance.name();
     assert.equal(name, 'AnteqToken', 'The name was not "AnteqToken".');
+  });
+
+  it('Should symbol to "ANQ".', async () => {
+    const symbol = await anteqTokenInstance.symbol();
+    assert.equal(symbol, 'ANQ', 'The name was not "ANQ".');
+  });
+
+  it('Should total suply equal to 1mln tokens.', async () => {
+    const totalSupply = await anteqTokenInstance.totalSupply();
+    assert.equal(
+      web3.utils.fromWei(totalSupply, 'ether'),
+      1000000,
+      'The total suply not equal to 1mln tokens.',
+    );
+  });
+
+  it('Should decimal equal to 18.', async () => {
+    const decimals = await anteqTokenInstance.decimals();
+    assert.equal(decimals, 18, 'The decimal was not equal to 18".');
+  });
+
+  it('Deplyer address should have 1mln tokens', async () => {
+    const deployerBalance = await anteqTokenInstance.balanceOf(accounts[0]);
+    console.log(web3.utils.fromWei(deployerBalance, 'ether'));
+
+    assert.equal(
+      web3.utils.fromWei(deployerBalance, 'ether'),
+      1000000,
+      'Deployer address should have 1mln tokens.',
+    );
   });
 });
