9baae48da3a19afea3daf7c7b95ac9cd5eb9eb96
Antek Kois <koisantek96@gmail.com>
2022-01-19 20:31:58 +0100

    add transfer and transferFrom method, transfer event and test that

--- contracts/AnteqToken.sol
+++ contracts/AnteqToken.sol
@@ -1,51 +1,85 @@
-pragma solidity >= 0.4.24 < 8.11.0;
+pragma solidity >=0.4.24 <8.11.0;
 
-contract AnteqToken{
+contract AnteqToken {
+    string private _name = "AnteqToken";
+    string private _symbol = "ANQ";
+    uint256 private _totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint8 private _decimals = 18;
 
-  string private _name = "AnteqToken";
-  string private _symbol = "ANQ";
-  uint256 private _totalSupply = 1000000000000000000000000; // 1 million tokens
-  uint8   private _decimals = 18;
+    mapping(address => uint256) private _balanceOf;
+    mapping(address => mapping(address => uint256)) private _allowance;
 
-  mapping(address => uint256) private _balanceOf;
-  mapping(address => mapping(address => uint256)) private _allowance;
+    constructor() public {
+        _balanceOf[msg.sender] = _totalSupply;
+    }
 
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
-  constructor() public {
-    _balanceOf[msg.sender] = _totalSupply;
-  }
+    function name() public view returns (string memory) {
+        return _name;
+    }
 
-  event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
 
-  function name() public view returns (string memory){
-    return _name;
-  }
-  
-  function symbol() public view returns (string memory){
-    return _symbol;
-  }
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
 
-  function totalSupply() public view returns (uint256){
-    return _totalSupply;
-  }
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
 
-  function decimals() public view returns (uint8){
-    return _decimals;
-  }
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return _balanceOf[_owner];
+    }
 
-  function balanceOf(address _owner) public view returns (uint256 balance){
-    return _balanceOf[_owner];
-  }
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(
+            _balanceOf[msg.sender] >= _value,
+            "Not enought token on sender address."
+        );
+        _balanceOf[msg.sender] = _balanceOf[msg.sender] - _value;
+        _balanceOf[_to] = _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 
-  function transfer(address _to, uint256 _value) public returns (bool success){
-    require(_balanceOf[msg.sender] >= _value, 'Not enought token on sender address.');
-    _balanceOf[msg.sender] = _balanceOf[msg.sender] - _value;
-    _balanceOf[_to] = _value;
-    return true;
-  }
-  // function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
-  // function approve(address _spender, uint256 _value) public returns (bool success)
-  // function allowance(address _owner, address _spender) public view returns (uint256 remaining)
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(
+            _allowance[_from][msg.sender] >= _value,
+            "Not allowance to sender to send this amount tokens"
+        );
+        require(
+            _balanceOf[_from] >= _value,
+            "Not enought token on _from address."
+        );
+        _balanceOf[_from] -= _value;
+        _balanceOf[_to] += _value;
+        _allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        _allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+    // function allowance(address _owner, address _spender) public view returns (uint256 remaining)
 }
--- test/anteqtoken.js
+++ test/anteqtoken.js
@@ -1,26 +1,29 @@
+const { assert } = require('chai');
+
 const AnteqToken = artifacts.require('./AnteqToken.sol');
-// const Web3 = require('./client/web3');
+
+require('chai').use(require('chai-as-promised')).should();
 
 contract('AnteqToken', (accounts) => {
-  let anteqTokenInstance;
+  let anteqToken;
 
   before(async () => {
-    anteqTokenInstance = await AnteqToken.new();
+    anteqToken = await AnteqToken.new();
   });
 
   describe('Deployed contract', async () => {
     it('Should name to "AnteqToken".', async () => {
-      const name = await anteqTokenInstance.name();
+      const name = await anteqToken.name();
       assert.equal(name, 'AnteqToken', 'The name was not "AnteqToken".');
     });
 
     it('Should symbol to "ANQ".', async () => {
-      const symbol = await anteqTokenInstance.symbol();
+      const symbol = await anteqToken.symbol();
       assert.equal(symbol, 'ANQ', 'The name was not "ANQ".');
     });
 
     it('Should total suply equal to 1mln tokens.', async () => {
-      const totalSupply = await anteqTokenInstance.totalSupply();
+      const totalSupply = await anteqToken.totalSupply();
       assert.equal(
         web3.utils.fromWei(totalSupply, 'ether'),
         1000000,
@@ -29,12 +32,12 @@ contract('AnteqToken', (accounts) => {
     });
 
     it('Should decimal equal to 18.', async () => {
-      const decimals = await anteqTokenInstance.decimals();
+      const decimals = await anteqToken.decimals();
       assert.equal(decimals, 18, 'The decimal was not equal to 18".');
     });
 
     it('Deplyer address should have 1mln tokens', async () => {
-      const deployerBalance = await anteqTokenInstance.balanceOf(accounts[0]);
+      const deployerBalance = await anteqToken.balanceOf(accounts[0]);
 
       assert.equal(
         web3.utils.fromWei(deployerBalance, 'ether'),
@@ -44,20 +47,80 @@ contract('AnteqToken', (accounts) => {
     });
   });
 
-  describe('Transfer tokens', async () => {
+  describe('Transfer tokens by "transfer" function', async () => {
     it('Deployer can send tokens to another address', async () => {
-      const amount = web3.utils.toBN(1000);
-      const to = accounts[1];
+      const { logs } = await anteqToken.transfer(
+        accounts[1],
+        web3.utils.toWei(web3.utils.toBN(1000), 'ether'),
+        {
+          from: accounts[0],
+        },
+      );
 
-      await anteqTokenInstance.transfer(to, web3.utils.toWei(amount, 'ether'), {
-        from: accounts[0],
-      });
+      assert.equal(logs[0].args._from, accounts[0]);
+      assert.equal(logs[0].args._to, accounts[1]);
+      assert.equal(web3.utils.fromWei(logs[0].args._value, 'ether'), 1000);
 
-      const balanseRecipient = await anteqTokenInstance.balanceOf(to);
+      const balanseRecipient = await anteqToken.balanceOf(accounts[1]);
       assert.equal(
         web3.utils.fromWei(balanseRecipient, 'ether'),
         1000,
         'Recipient balanse is right after transfer',
+      );
+    });
+    it("Sender doesn't do transfer if havn't enought token", async () => {
+      await anteqToken.transfer(accounts[0], web3.utils.toWei(web3.utils.toBN(1001), 'ether'), {
+        from: accounts[1],
+      }).should.be.rejected;
+    });
+  });
+
+  describe('Transfer tokens by "transferFrom" function', async () => {
+    it("Sender can't transfer token from another address if havn't allowance", async () => {
+      await anteqToken.transferFrom(
+        accounts[1],
+        accounts[2],
+        web3.utils.toWei(web3.utils.toBN(1000), 'ether'),
+        {
+          from: accounts[0],
+        },
+      ).should.be.rejected;
+    });
+
+    it("Sender can't transfer token from another address if want send to much than allowance approw", async () => {
+      await anteqToken.approve(accounts[0], web3.utils.toWei(web3.utils.toBN(1000), 'ether'), {
+        from: accounts[1],
+      });
+
+      await anteqToken.transferFrom(
+        accounts[1],
+        accounts[2],
+        web3.utils.toWei(web3.utils.toBN(1001), 'ether'),
+        {
+          from: accounts[0],
+        },
+      ).should.be.rejected;
+    });
+
+    it('Sender can transfer token from another address if have allowance', async () => {
+      const { logs } = await anteqToken.transferFrom(
+        accounts[1],
+        accounts[2],
+        web3.utils.toWei(web3.utils.toBN(1000), 'ether'),
+        {
+          from: accounts[0],
+        },
+      );
+
+      assert.equal(logs[0].args._from, accounts[1]);
+      assert.equal(logs[0].args._to, accounts[2]);
+      assert.equal(web3.utils.fromWei(logs[0].args._value, 'ether'), 1000);
+
+      const balance = await anteqToken.balanceOf(accounts[2]);
+      assert.equal(
+        web3.utils.fromWei(balance),
+        1000,
+        'Tokens can be transfer by another address by allowance',
       );
     });
   });
