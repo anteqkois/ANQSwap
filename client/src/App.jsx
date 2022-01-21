40fd8b1bf980c559832d2944790913e1e9d7c1dc
Antek Kois <koisantek96@gmail.com>
2022-01-22 00:07:24 +0100

    update method to connect metamask

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ANQSwap from './contracts/ANQSwap.json';
 import AnteqToken from './contracts/AnteqToken.json';
-import getWeb3 from './helpers/getWeb3';
-
+import Web3 from 'web3';
 import './App.css';
 
 export const App = () => {
@@ -11,22 +10,25 @@ export const App = () => {
   const [ANQSwapContract, setANQSwapContract] = useState(null);
   const [ANQContract, setANQContract] = useState(null);
 
-  useEffect(async () => {
+  const handleConnect = async () => {
     try {
-      const web3 = await getWeb3();
+      if (!window.ethereum) throw new Error('No crypto wallet found. Please install it.');
+
+      await window.ethereum.send('eth_requestAccounts');
+      const web3 = await new Web3(window.ethereum);
       setWeb3(web3);
 
       setAccounts(await web3.eth.getAccounts());
-
       const networkId = await web3.eth.net.getId();
 
       setANQSwapContract(new web3.eth.Contract(ANQSwap.abi, ANQSwap.networks[networkId].address));
       setANQContract(new web3.eth.Contract(AnteqToken.abi, AnteqToken.networks[networkId].address));
     } catch (error) {
-      console.log(error);
       alert(`Failed to load web3, accounts, or contract. Check console for details.`);
     }
-  }, []);
+  };
+
+  const handleDisconnect = async () => {};
 
   const runExample = async () => {
     const balanceANQInSwap = await ANQContract.methods.balanceOf(ANQSwapContract.options.address).call();
@@ -37,7 +39,10 @@ export const App = () => {
 
   return (
     <div>
+      <button onClick={handleConnect}>Connect Wallet</button>
+      <button onClick={handleDisconnect}>Disconnect Wallet</button>
       <button onClick={runExample}>Run Example !</button>
+      <h1 className="text-3xl font-bold underline text-cyan-800">Hello world!!!!!</h1>
     </div>
   );
 };
