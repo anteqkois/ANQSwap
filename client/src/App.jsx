fb10ee5997ab5980a45a4afbe9716667fef34396
Antek Kois <koisantek96@gmail.com>
2022-01-21 14:15:32 +0100

    Connect to metamask on Frontend and test contracts if they wroks right

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,72 +1,45 @@
-import React, { Component } from 'react';
-import SimpleStorageContract from './contracts/SimpleStorage.json';
+import React, { useEffect, useState } from 'react';
+import ANQSwap from './contracts/ANQSwap.json';
+import AnteqToken from './contracts/AnteqToken.json';
 import getWeb3 from './helpers/getWeb3';
 
 import './App.css';
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+export const App = () => {
+  const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState(null);
+  const [ANQSwapContract, setANQSwapContract] = useState(null);
+  const [ANQContract, setANQContract] = useState(null);
 
-  componentDidMount = async () => {
+  useEffect(async () => {
     try {
-      // Get network provider and web3 instance.
       const web3 = await getWeb3();
+      setWeb3(web3);
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      setAccounts(await web3.eth.getAccounts());
 
-      // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance });
-      
-      this.runExample();
+      setANQSwapContract(new web3.eth.Contract(ANQSwap.abi, ANQSwap.networks[networkId].address));
+      setANQContract(new web3.eth.Contract(AnteqToken.abi, AnteqToken.networks[networkId].address));
     } catch (error) {
-      // Catch any errors for any of the above operations.
+      console.log(error);
       alert(`Failed to load web3, accounts, or contract. Check console for details.`);
     }
+  }, []);
+
+  const runExample = async () => {
+    const balanceANQInSwap = await ANQContract.methods.balanceOf(ANQSwapContract.options.address).call();
+    const balanceANQInUser = await ANQContract.methods.balanceOf(accounts[0]).call();
+    console.log(balanceANQInSwap);
+    console.log(balanceANQInUser);
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show a stored value of 5 (by
-          default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <button onClick={runExample}>Run Example !</button>
+    </div>
+  );
+};
 
 export default App;
