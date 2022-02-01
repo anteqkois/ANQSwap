0ffc00d2a8d7ddc22a02ff62a9bd6c9048c746b4
Antek Kois <koisantek96@gmail.com>
2022-02-01 20:34:27 +0100

    Add prietter config to .sol, sell function (wrong ETH amount send back)

--- contracts/ANQSwap.sol
+++ contracts/ANQSwap.sol
@@ -6,191 +6,115 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./AnteqToken.sol";
 
 contract ANQSwap is Ownable {
-    using SafeMath for uint256;
+	using SafeMath for uint256;
 
-    AnteqToken public anteqToken;
-    uint256 public totalLiquidityFormula;
-    string public name = "AnteqToken Swap";
+	AnteqToken public anteqToken;
+	uint256 public totalLiquidityFormula;
+	string public name = "AnteqToken Swap";
 
-    constructor(AnteqToken _token) {
-        anteqToken = _token;
-    }
+	constructor(AnteqToken _token) {
+		anteqToken = _token;
+	}
 
-    event BuyTokens(address indexed _buyer, uint256 _amount);
-    event SellTokens(address indexed _seller, uint256 _amount);
-    event debugN(string _info, uint256 _number);
+	event BuyTokens(address indexed _buyer, uint256 _amount);
+	event SellTokens(address indexed _seller, uint256 _amount);
+	event debugN(string _info, uint256 _number);
 
-    function addInitialLiquidity(uint256 _tokenAmount) external payable {
-        require(
-            _tokenAmount <= anteqToken.balanceOf(msg.sender),
-            "Not enought ANQ on wallet"
-        );
-        anteqToken.transferFrom(msg.sender, address(this), _tokenAmount);
-        totalLiquidityFormula = _tokenAmount.mul(msg.value);
-    }
+	function addInitialLiquidity(uint256 _tokenAmount) external payable {
+		require(_tokenAmount <= anteqToken.balanceOf(msg.sender), "Not enought ANQ on wallet.");
+		anteqToken.transferFrom(msg.sender, address(this), _tokenAmount);
+		totalLiquidityFormula = _tokenAmount.mul(msg.value);
+	}
 
-    function changeTokenAddress(AnteqToken _token)
-        external
-        onlyOwner
-        returns (bool success)
-    {
-        anteqToken = _token;
-        return true;
-    }
+	function changeTokenAddress(AnteqToken _token) external onlyOwner returns (bool success) {
+		anteqToken = _token;
+		return true;
+	}
 
-    function predirectAmountOfBuyTokens(uint256 _ETHAmount)
-        public
-        view
-        returns (uint256 predirectANQAmount)
-    {
-        uint256 amountANQToTransfer = _getAmountOut(
-            _ETHAmount,
-            address(this).balance,
-            anteqToken.balanceOf(address(this))
-        );
-        // uint256 amountANQToTransfer = (anteqToken.balanceOf(address(this)) *
-        //     _ETHAmount) / (address(this).balance + _ETHAmount);
-        require(
-            anteqToken.balanceOf(address(this)) >= amountANQToTransfer,
-            "AnteqToken Swap havn't enought ANQ."
-        );
-        return amountANQToTransfer;
-    }
+	function predirectAmountOfBuyTokens(uint256 _ETHAmount) public view returns (uint256 predirectANQAmount) {
+		uint256 amountANQToTransfer = _getAmountOut(_ETHAmount, address(this).balance, anteqToken.balanceOf(address(this)));
+		// uint256 amountANQToTransfer = (anteqToken.balanceOf(address(this)) *
+		//     _ETHAmount) / (address(this).balance + _ETHAmount);
+		require(anteqToken.balanceOf(address(this)) >= amountANQToTransfer, "AnteqToken Swap havn't enought ANQ.");
+		return amountANQToTransfer;
+	}
 
-    function _getAmountOut(
-        uint256 _amountTokenIn,
-        uint256 _reserveIn,
-        uint256 _reserveOut
-    ) internal pure returns (uint256 amountTokenOut) {
-        require(_amountTokenIn > 0, "Amount of tokens in can't be equal 0");
-        uint256 amountInWithoutFee = _amountTokenIn.mul(1000); // With fee 0,3% ==> mul(997)
-        uint256 numerator = amountInWithoutFee.mul(_reserveOut);
-        uint256 denominator = _reserveIn.mul(1000).add(amountInWithoutFee);
-        amountTokenOut = numerator.div(denominator);
-    }
+	function _getAmountOut(
+		uint256 _amountTokenIn,
+		uint256 _reserveIn,
+		uint256 _reserveOut
+	) internal pure returns (uint256 amountTokenOut) {
+		require(_amountTokenIn > 0, "Amount of tokens in can't be equal 0.");
 
-    function buyTokens() external payable {
-        uint256 amountANQToTransfer = _getAmountOut(
-            msg.value,
-            address(this).balance.sub(msg.value),
-            anteqToken.balanceOf(address(this))
-        );
+		// amountTokenOut = (_reserveOut * _amountTokenIn) / (_reserveIn + _amountTokenIn);
 
-        uint256 amountANQInSwapAfterTransfer = anteqToken.balanceOf(
-            address(this)
-        ).sub(amountANQToTransfer);
+		uint256 amountInWithoutFee = _amountTokenIn.mul(1000); // With fee 0,3% ==> mul(997)
+		uint256 numerator = amountInWithoutFee.mul(_reserveOut);
+		uint256 denominator = _reserveIn.mul(1000).add(amountInWithoutFee);
+		amountTokenOut = numerator.div(denominator);
+	}
 
-        require(
-            amountANQInSwapAfterTransfer.mul(address(this).balance) >=
-                totalLiquidityFormula,
-            "Invalid final tokens amount"
-        );
-        totalLiquidityFormula = amountANQInSwapAfterTransfer.mul(
-            address(this).balance
-        );
+	function buyTokens() external payable {
+		uint256 amountANQToTransfer = _getAmountOut(
+			msg.value,
+			address(this).balance.sub(msg.value),
+			anteqToken.balanceOf(address(this))
+		);
 
-        anteqToken.transfer(msg.sender, amountANQToTransfer);
-        emit BuyTokens(msg.sender, amountANQToTransfer);
-    }
-    // function buyTokens() external payable {
-    //     uint256 amountANQToTransfer = (anteqToken.balanceOf(address(this)) *
-    //         msg.value) / (address(this).balance);
-    //     require(
-    //         anteqToken.balanceOf(address(this)) >= amountANQToTransfer,
-    //         "AnteqToken Swap havn't enought ANQ."
-    //     );
-    //     uint256 amountANQInSwapAfterBuy = (anteqToken.balanceOf(address(this)) -
-    //         amountANQToTransfer);
+		uint256 amountANQInSwapAfterTransfer = anteqToken.balanceOf(address(this)).sub(amountANQToTransfer);
 
-    //     uint256 newTotalLiquidityFormula = (amountANQInSwapAfterBuy * (address(this).balance));
+		require(
+			amountANQInSwapAfterTransfer.mul(address(this).balance) >= totalLiquidityFormula,
+			"Invalid final tokens amount."
+		);
+		totalLiquidityFormula = amountANQInSwapAfterTransfer.mul(address(this).balance);
 
-    //     // uint256 diffLiquidityFormula = totalLiquidityFormula - newTotalLiquidityFormula;
+		anteqToken.transfer(msg.sender, amountANQToTransfer);
+		emit BuyTokens(msg.sender, amountANQToTransfer);
+	}
 
-    //     // uint256 newANQInSwap = (totalLiquidityFormula - diffLiquidityFormula)l;
+	function sellTokens(uint256 _value) external {
+		require(anteqToken.balanceOf(msg.sender) >= _value, "You havn't enought ANQ tokens.");
+		uint256 amountETHToTransfer = _getAmountOut(_value, anteqToken.balanceOf(address(this)), address(this).balance);
+		require(address(this).balance >= amountETHToTransfer, "Swap doesn't have enought Ether to buy yours ANQ tokens.");
 
-    //     emit debugN("balanceANQOfSwap", anteqToken.balanceOf(address(this)));
-    //     emit debugN("anteqToken.balanceOf(address(this)) * msg.value", anteqToken.balanceOf(address(this)) *
-    //         msg.value);
-    //     emit debugN("ETH in Swap", (address(this).balance));
+		uint256 amountANQInSwapAfterTransfer = anteqToken.balanceOf(address(this)).add(_value);
 
-    //     emit debugN("liq before purchase", totalLiquidityFormula);
-    //     emit debugN("liq after purchase", newTotalLiquidityFormula);
-    //     emit debugN("ANQ amount In Swap", anteqToken.balanceOf(address(this)));
-    //     emit debugN("amountANQInSwapAfterBuy", amountANQInSwapAfterBuy);
-    //     emit debugN("amountBuyANQ", amountANQToTransfer);
+		require(
+			amountANQInSwapAfterTransfer.mul(address(this).balance - amountETHToTransfer) >= totalLiquidityFormula,
+			"Invalid final tokens amount."
+		);
 
-    //     emit debugN("noweANQ", (totalLiquidityFormula - (newTotalLiquidityFormula - totalLiquidityFormula))/address(this).balance);
-    //     emit debugN("noweK", ((totalLiquidityFormula - (newTotalLiquidityFormula - totalLiquidityFormula))/address(this).balance)*address(this).balance);
+		emit SellTokens(msg.sender, _value);
+		emit debugN("new K", amountANQInSwapAfterTransfer.mul(address(this).balance - amountETHToTransfer));
 
-    //     // require(
-    //     //     amountANQInSwapAfterBuy * address(this).balance ==
-    //     //         totalLiquidityFormula,
-    //     //     "Wrong total liquidity after purchase"
-    //     // );
+		anteqToken.transferFrom(msg.sender, address(this), _value);
+		payable(msg.sender).transfer(amountETHToTransfer);
 
-    //     // anteqToken.transfer(msg.sender, amountANQToTransfer);
-    //     emit BuyTokens(msg.sender, amountANQToTransfer);
-    // }
+		//TODO set new K
+		emit debugN("amountETHToTransfer", amountETHToTransfer);
+		emit debugN("amountANQInSwapAfterTransfer", amountANQInSwapAfterTransfer);
+		emit debugN("old K", totalLiquidityFormula);
+		emit debugN("ETH i nSwao after", address(this).balance);
+	}
 
-    // function buyTokens() external payable {
-    //     // uint256 amountANQAfterBuy = totalLiquidityFormula /
-    //     //     address(this).balance;
-    //     uint256 amountANQToTransfer = (anteqToken.balanceOf(address(this)) *
-    //         msg.value) / (address(this).balance);
-    //     require(
-    //         anteqToken.balanceOf(address(this)) >= amountANQToTransfer,
-    //         "AnteqToken Swap havn't enought ANQ."
-    //     );
-    //     uint256 amountANQInSwapAfterBuy = (anteqToken.balanceOf(address(this)) -
-    //         amountANQToTransfer);
+	// function sellTokens(uint256 _value) public {
+	//     require(
+	//         anteqToken.balanceOf(msg.sender) >= _value,
+	//         "You doesn't have enought AnteqToken."
+	//     );
+	//     uint256 etherToSendBack = _value / rate;
+	//     require(
+	//         address(this).balance >= etherToSendBack,
+	//         "AnteqToken Swap doesn't have enought Ether to buy yours token."
+	//     );
+	//     anteqToken.transferFrom(msg.sender, address(this), _value);
+	//     payable(msg.sender).transfer(etherToSendBack);
+	//     emit SellTokens(msg.sender, _value);
+	// }
 
-    //     uint256 newTotalLiquidityFormula = (amountANQInSwapAfterBuy * (address(this).balance));
-    //     uint256 diffLiquidityFormula = totalLiquidityFormula - newTotalLiquidityFormula;
-
-    //     uint256 newANQInSwap = (totalLiquidityFormula - diffLiquidityFormula)l;
-
-    //     emit debugN("liq after purchase", newTotalLiquidityFormula);
-    //     emit debugN("ANQ amount In Swap", anteqToken.balanceOf(address(this)));
-    //     emit debugN("amountANQInSwapAfterBuy", amountANQInSwapAfterBuy);
-    //     emit debugN("ETH amount in Swap", address(this).balance);
-    //     emit debugN("amountBuyANQ", amountANQToTransfer);
-
-    //     // require(
-    //     //     amountANQInSwapAfterBuy * address(this).balance ==
-    //     //         totalLiquidityFormula,
-    //     //     "Wrong total liquidity after purchase"
-    //     // );
-
-    //     anteqToken.transfer(msg.sender, amountANQToTransfer);
-    //     emit BuyTokens(msg.sender, amountANQToTransfer);
-    // }
-    // function buyTokens() external payable {
-    //     uint256 amountANQ = msg.value * rate;
-    //     require(
-    //         anteqToken.balanceOf(address(this)) >= amountANQ,
-    //         "AnteqToken Swap havn't enought ANQ."
-    //     );
-    //     anteqToken.transfer(msg.sender, amountANQ);
-    //     emit BuyTokens(msg.sender, amountANQ);
-    // }
-
-    // function sellTokens(uint256 _value) public {
-    //     require(
-    //         anteqToken.balanceOf(msg.sender) >= _value,
-    //         "You doesn't have enought AnteqToken."
-    //     );
-    //     uint256 etherToSendBack = _value / rate;
-    //     require(
-    //         address(this).balance >= etherToSendBack,
-    //         "AnteqToken Swap doesn't have enought Ether to buy yours token."
-    //     );
-    //     anteqToken.transferFrom(msg.sender, address(this), _value);
-    //     payable(msg.sender).transfer(etherToSendBack);
-    //     emit SellTokens(msg.sender, _value);
-    // }
-
-    // ADD WITHDRAW FUNCION
-    // function withdraw() external onlyOwner returns (bool success){
-    //     payable(owner().address).transfer();
-    // }
+	// ADD WITHDRAW FUNCION
+	// function withdraw() external onlyOwner returns (bool success){
+	//     payable(owner().address).transfer();
+	// }
 }
--- test/ANQSwap.test.js
+++ test/ANQSwap.test.js
@@ -6,105 +6,107 @@ const ANQSwap = artifacts.require('./ANQSwap.sol');
 require('chai').use(require('chai-as-promised')).should();
 
 contract('ANQSwap', (accounts) => {
-  let anteqToken, anqSwap;
+	let anteqToken, anqSwap;
 
-  before(async () => {
-    anteqToken = await AnteqToken.new();
-    anqSwap = await ANQSwap.new(anteqToken.address);
+	before(async () => {
+		anteqToken = await AnteqToken.new();
+		anqSwap = await ANQSwap.new(anteqToken.address);
 
-    await anteqToken.approve(anqSwap.address, web3.utils.toWei(web3.utils.toBN(100_000), 'ether'), { from: accounts[0] });
+		await anteqToken.approve(anqSwap.address, web3.utils.toWei(web3.utils.toBN(100_000), 'ether'), { from: accounts[0] });
 
-    await anqSwap.addInitialLiquidity(web3.utils.toWei(web3.utils.toBN(100_000), 'ether'), {
-      from: accounts[0],
-      value: web3.utils.toWei(web3.utils.toBN(50), 'ether'),
-    });
-  });
+		await anqSwap.addInitialLiquidity(web3.utils.toWei(web3.utils.toBN(100_000), 'ether'), {
+			from: accounts[0],
+			value: web3.utils.toWei(web3.utils.toBN(50), 'ether'),
+		});
+	});
 
-  describe('Deploy ANQSwap', async () => {
-    it('ANQSwap had got assigned right token address', async () => {
-      const tokenAddressInSwap = await anqSwap.anteqToken();
-      assert.equal(anteqToken.address, tokenAddressInSwap);
-    });
-  });
+	describe('Deploy ANQSwap', async () => {
+		it('ANQSwap had got assigned right token address', async () => {
+			const tokenAddressInSwap = await anqSwap.anteqToken();
+			assert.equal(anteqToken.address, tokenAddressInSwap);
+		});
+	});
 
-  describe('Initial liquidity, ANQ and ETH amount', async () => {
-    it('Right amount of totalLiquidityFormula', async () => {
-      const totalLiquidityFormula = await anqSwap.totalLiquidityFormula();
+	describe('Initial liquidity, ANQ and ETH amount', async () => {
+		it('Right amount of totalLiquidityFormula', async () => {
+			const totalLiquidityFormula = await anqSwap.totalLiquidityFormula();
 
-      // totalLiquidityFormula = x**10*18 * y**10*18 ==>  totalLiquidityFormula*10*19
-      assert.equal(web3.utils.fromWei(web3.utils.fromWei(totalLiquidityFormula, 'ether'), 'ether'), 5_000_000);
-    });
-    it('Right amount of ANQ', async () => {
-      const balanceOfSwap = await anteqToken.balanceOf(anqSwap.address);
-      assert.equal(web3.utils.fromWei(balanceOfSwap, 'ether'), 100_000);
-    });
-    it('Right amount of ETH', async () => {
-      const balanceOfSwap = await web3.eth.getBalance(anqSwap.address);
-      assert.equal(web3.utils.fromWei(balanceOfSwap, 'ether'), 50);
-    });
-  });
+			// totalLiquidityFormula = x**10*18 * y**10*18 ==>  totalLiquidityFormula*10*19
+			assert.equal(web3.utils.fromWei(web3.utils.fromWei(totalLiquidityFormula, 'ether'), 'ether'), 5_000_000);
+		});
+		it('Right amount of ANQ', async () => {
+			const balanceOfSwap = await anteqToken.balanceOf(anqSwap.address);
+			assert.equal(web3.utils.fromWei(balanceOfSwap, 'ether'), 100_000);
+		});
+		it('Right amount of ETH', async () => {
+			const balanceOfSwap = await web3.eth.getBalance(anqSwap.address);
+			assert.equal(web3.utils.fromWei(balanceOfSwap, 'ether'), 50);
+		});
+	});
 
-  describe('Change ANQ address', async () => {
-    it('Owner can change ANQ address', async () => {
-      await anqSwap.changeTokenAddress(accounts[2], { from: accounts[0] });
-      const tokenAddressInSwap = await anqSwap.anteqToken();
-      assert.notEqual(anteqToken.address, tokenAddressInSwap);
-      await anqSwap.changeTokenAddress(anteqToken.address, { from: accounts[0] });
-    });
+	describe('Change ANQ address', async () => {
+		it('Owner can change ANQ address', async () => {
+			await anqSwap.changeTokenAddress(accounts[2], { from: accounts[0] });
+			const tokenAddressInSwap = await anqSwap.anteqToken();
+			assert.notEqual(anteqToken.address, tokenAddressInSwap);
+			await anqSwap.changeTokenAddress(anteqToken.address, { from: accounts[0] });
+		});
 
-    it("Other address can't change ANQ address", async () => {
-      await anqSwap.changeTokenAddress(accounts[2], { from: accounts[2] }).should.be.rejected;
-    });
-  });
+		it("Other address can't change ANQ address", async () => {
+			await anqSwap.changeTokenAddress(accounts[2], { from: accounts[2] }).should.be.rejected;
+		});
+	});
 
-  describe('Buy token', async () => {
-    it('User can buy ANQ', async () => {
-      const predirectAmountOfBuyTokens = await anqSwap.predirectAmountOfBuyTokens(web3.utils.toWei(web3.utils.toBN(5), 'ether'));
-      // console.log('predirectAmountOfBuyTokens', web3.utils.fromWei(predirectAmountOfBuyTokens));
+	describe('Buy token', async () => {
+		it('User can buy ANQ', async () => {
+			const predirectAmountOfBuyTokens = await anqSwap.predirectAmountOfBuyTokens(web3.utils.toWei(web3.utils.toBN(5), 'ether'));
 
-      const { logs } = await anqSwap.buyTokens({ from: accounts[1], value: web3.utils.toWei(web3.utils.toBN(5), 'ether') });
+			const { logs } = await anqSwap.buyTokens({ from: accounts[1], value: web3.utils.toWei(web3.utils.toBN(5), 'ether') });
 
-      // for (const log of logs) {
-      //   console.log(log.args._info, web3.utils.fromWei(log.args._number, 'wei'));
-      // }
+			// for (const log of logs) {
+			//   console.log(log.args._info, web3.utils.fromWei(log.args._number, 'wei'));
+			// }
 
-      const buyerBalanceOfANQ = await anteqToken.balanceOf(accounts[1]);
-      const swapBalanceOfANQ = await anteqToken.balanceOf(anqSwap.address);
-      const swapBalanceOfETH = await web3.eth.getBalance(anqSwap.address);
+			const buyerBalanceOfANQ = await anteqToken.balanceOf(accounts[1]);
+			const swapBalanceOfANQ = await anteqToken.balanceOf(anqSwap.address);
+			const swapBalanceOfETH = await web3.eth.getBalance(anqSwap.address);
 
-      assert.equal(logs[0].args._buyer, accounts[1]);
-      assert.equal(web3.utils.fromWei(logs[0].args._amount), web3.utils.fromWei(predirectAmountOfBuyTokens));
+			assert.equal(logs[0].args._buyer, accounts[1]);
+			assert.equal(web3.utils.fromWei(logs[0].args._amount), web3.utils.fromWei(predirectAmountOfBuyTokens));
 
+			assert.equal(web3.utils.fromWei(buyerBalanceOfANQ), web3.utils.fromWei(predirectAmountOfBuyTokens));
+			assert.equal(web3.utils.fromWei(swapBalanceOfANQ), 100_000 - web3.utils.fromWei(predirectAmountOfBuyTokens));
+			assert.equal(web3.utils.fromWei(swapBalanceOfETH), 55);
+		});
 
-      //potem odkomentować 
-      assert.equal(web3.utils.fromWei(buyerBalanceOfANQ), web3.utils.fromWei(predirectAmountOfBuyTokens));
-      assert.equal(web3.utils.fromWei(swapBalanceOfANQ), 100_000 - web3.utils.fromWei(predirectAmountOfBuyTokens));
-      assert.equal(web3.utils.fromWei(swapBalanceOfETH), 55);
-    });
+		it("User can't buy token if havn't enought ether", async () => {
+			await anqSwap.buyTokens({ from: accounts[0], value: web3.utils.toWei(web3.utils.toBN(1000)) }).should.be.rejected;
+		});
+	});
 
-    it("User can't buy token if havn't enought ether", async () => {
-      await anqSwap.buyTokens({ from: accounts[0], value: web3.utils.toWei(web3.utils.toBN(1000)) }).should.be.rejected;
-    });
-  });
+	describe('Sell token', async () => {
+		it('User can sell ANQ', async () => {
+      //TODO Add predirect
+			const balanceOfANQSeller = await anteqToken.balanceOf(accounts[1]);
 
-  xdescribe('Sell token', async () => {
-    it('User can sell token', async () => {
-      await anteqToken.approve(anqSwap.address, web3.utils.toWei(web3.utils.toBN(100), 'ether'), { from: accounts[0] });
-      const { logs } = await anqSwap.sellTokens(web3.utils.toWei(web3.utils.toBN(100), 'ether'), { from: accounts[0] });
+			await anteqToken.approve(anqSwap.address, balanceOfANQSeller, {
+				from: accounts[1],
+			});
+			const { logs } = await anqSwap.sellTokens(balanceOfANQSeller, { from: accounts[1] });
 
-      const balanceOfUserANQ = await anteqToken.balanceOf(accounts[0]);
-      const balanceOfSwapANQ = await anteqToken.balanceOf(anqSwap.address);
-      const balanceOfSwapEther = await web3.eth.getBalance(anqSwap.address);
+			const balanceOfUserANQ = await anteqToken.balanceOf(accounts[1]);
+			const balanceOfSwapANQ = await anteqToken.balanceOf(anqSwap.address);
+			const balanceOfSwapEther = await web3.eth.getBalance(anqSwap.address);
 
-      assert.equal(logs[0].args._seller, accounts[0]);
-      assert.equal(web3.utils.fromWei(logs[0].args._amount, 'ether'), 100);
-      assert.equal(web3.utils.fromWei(balanceOfUserANQ, 'ether'), 0);
-      assert.equal(web3.utils.fromWei(balanceOfSwapANQ, 'ether'), 1000000);
-      assert.equal(web3.utils.fromWei(balanceOfSwapEther, 'ether'), 0);
-    });
+			assert.equal(logs[0].args._seller, accounts[1]);
+			assert.equal(web3.utils.fromWei(logs[0].args._amount, 'ether'), web3.utils.fromWei(balanceOfANQSeller));
+			assert.equal(web3.utils.fromWei(balanceOfUserANQ, 'ether'), 0);
+			assert.equal(web3.utils.fromWei(balanceOfSwapANQ, 'ether'), 100000);
+			// assert.equal(web3.utils.fromWei(balanceOfSwapEther, 'ether'), 0);
+		});
 
-    it("User cann't sell token if havn't enought", async () => {
-      await anqSwap.sellTokens(web3.utils.toWei(web3.utils.toBN(100), 'ether'), { from: accounts[0] }).should.be.rejected;
-    });
-  });
+		it("User cann't sell token if havn't enought", async () => {
+			await anqSwap.sellTokens(web3.utils.toWei(web3.utils.toBN(100), 'ether'), { from: accounts[0] }).should.be.rejected;
+		});
+	});
 });
